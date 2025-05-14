134cbe585d5b02a63b590a87539733c0fcfe7fa0
AstraviaComics <astraviacomic@gmail.com>
2025-05-14 13:08:51 +0700

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,18 +1,3 @@
-export default async function handler(req, res) {
-  const crypto = await import("crypto");
-
-  const privateKey = process.env.IMAGEKIT_PRIVATE_KEY;
-  const publicKey = process.env.IMAGEKIT_PUBLIC_KEY;
-
-  const timestamp = Math.floor(Date.now() / 1000);
-  const signature = crypto
-    .createHash("sha1")
-    .update(timestamp + privateKey)
-    .digest("hex");
-
-  res.status(200).json({
-    signature,
-    expire: timestamp,
-    token: publicKey
-  });
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello from ImageKit Auth API!" });
 }
