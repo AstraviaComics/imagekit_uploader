27d3a138c7a0deb2c8928830763f7c5d653468c2
AstraviaComics <astraviacomic@gmail.com>
2025-05-14 12:51:45 +0700

    Create auth.js

---
+++ api/auth.js
@@ -0,0 +1,19 @@
+// api/auth.js
+
+export default async function handler(req, res) {
+  const crypto = await import("crypto");
+
+  const privateKey = process.env.IMAGEKIT_PRIVATE_KEY;
+
+  const timestamp = Math.floor(Date.now() / 1000);
+  const signature = crypto
+    .createHash("sha1")
+    .update(timestamp + privateKey)
+    .digest("hex");
+
+  res.status(200).json({
+    signature,
+    expire: timestamp,
+    token: process.env.IMAGEKIT_PUBLIC_KEY
+  });
+}
